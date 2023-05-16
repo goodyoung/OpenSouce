1f2378c73452b397a572029735eaded1c4918fcf
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-05-17 00:27:48 +0900

    Update map.js

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,11 +1,3 @@
-//JSON파일 불러오는 함수
-fetch("./json_data.json")
-    .then((res) => res.json())
-    .then((stations) => {
-        console.log(stations);
-        console.log(stations.stationLatitude[0]);
-    })
-
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.576022, 126.9769), // 지도의 중심좌표
@@ -19,30 +11,71 @@ console.log(`1`);
 var geocoder = new kakao.maps.services.Geocoder();
 console.log(`2`);
 
-// 현재위치로 좌표를 검색합니다
-geocoder.addressSearch('서울 종로구 사직로 161', function(result, status) {
-    // 정상적으로 검색이 완료됐으면
-	console.log(`3`);
-	console.log(status);
-	console.log(kakao.maps.services.Status.OK);
-     if (status === kakao.maps.services.Status.OK) {
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: coords
-        });
-
-        // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-            content: '<div style="width:150px;text-align:center;padding:6px 0;">현재위치</div>'
-        });
-        infowindow.open(map, marker);
-
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        map.setCenter(coords);
-    }else{
-		console.log(`fail`);
+function getAddressCoordinates(address) {
+	return new Promise((resolve, reject) => {
+	  geocoder.addressSearch(address, function(result, status) {
+		if (status === kakao.maps.services.Status.OK) {
+		  var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+		  resolve(coords);
+		} else {
+		  reject(new Error("주소 검색에 실패했습니다."));
+		}
+	  });
+	});
+  }
+  
+  async function searchAddress() {
+	try {
+	  console.log(`3`);
+	  const address = '서울 종로구 사직로 161';
+	  const coords = await getAddressCoordinates(address);
+  
+	  // 결과값으로 받은 위치를 마커로 표시합니다
+	  var marker = new kakao.maps.Marker({
+		map: map,
+		position: coords
+	  });
+  
+	  // 인포윈도우로 장소에 대한 설명을 표시합니다
+	  var infowindow = new kakao.maps.InfoWindow({
+		content: '<div style="width:150px;text-align:center;padding:6px 0;">현재위치</div>'
+	  });
+	  infowindow.open(map, marker);
+  
+	  // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+	  map.setCenter(coords);
+	} catch (error) {
+	  console.error(error);
 	}
-}); 
+  }
+  
+  searchAddress();
+  
+
+var clusterer = new kakao.maps.MarkerClusterer({
+	map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+	averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+	minLevel: 6 // 클러스터 할 최소 지도 레벨 
+});
+console.log(`4`);
+
+
+$.get("./json_data.json", function(data) {
+    var stationLatitude = data.stationLatitude;
+    var stationLongitude = data.stationLongitude;
+
+    // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+    // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+    var markers = $.map(stationLatitude, function(latitude, index) {
+        var longitude = stationLongitude[index];
+        var position = new kakao.maps.LatLng(latitude, longitude);
+        return new kakao.maps.Marker({ position: position });
+    });
+
+    // 클러스터러에 마커들을 추가합니다
+    clusterer.addMarkers(markers);
+});
+
+
+
+
